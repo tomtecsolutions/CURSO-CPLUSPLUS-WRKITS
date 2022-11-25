2b2108d17ea36867f0c19e4a3e60f864a8459ed0
Wellington Dorta <wellington.dorta@gmail.com>
2022-11-24 21:49:23 -0300

    Aula 1 Hello World

---
+++ Modulo1/Aula1/Hello World.cpp
@@ -0,0 +1,18 @@
+// Curso C++ WR Kits
+// Autor: Wellington Dorta
+// Aula 0: Hello World
+
+
+
+
+#include <iostream>
+
+int main()
+{
+    /* code */
+
+    std::cout<< "Hello World";
+
+
+    return 0;
+}
